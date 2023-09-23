ec8451cbc3ee5c83071934b7e7924077e89352cd
Joey Marino <marinojoey@pm.me>
2023-09-22 20:43:51 -0600

    Bare repo with ESLint and Prettier configured

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter, Hanuman } from 'next/font/google'
-import NavBar from './components/Navbar'
-import Footer from './components/Footer'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-const hanuman = Hanuman({ subsets: ['latin'], style: 'normal', weight: '300' })
 
 export const metadata: Metadata = {
   title: 'Personal Site',
@@ -16,12 +13,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <NavBar />
-        <main className={`${hanuman.className} flex-grow pl-0 mt-0 mb-0 test-homeText`}>
-          {children}
-        </main>
-        <Footer className="flex items-center justify-center" />
+      <body className={`${inter.className}`}>
+        <main>{children}</main>
       </body>
     </html>
   )
